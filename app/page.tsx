e6a488afa3848b263e9fac72e0e6ddc5a316827b
Juan Cruz Cortiñas <juancruzcor@hotmail.com>
2024-01-26 14:37:32 -0300

    Changed background color from light mode, created NavBar component to factor main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,14 @@
 import { ModeToggle } from "@/components/ModeToggle";
+import { NavBar } from "@/components/NavBar";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-16">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <Image
-          src="/logogym.png"
-          alt="Jc personal trainer Logo"
-          className="dark:invert w-16 lg:w-24"
-          width={100}
-          height={24}
-          priority
-        />
-        <ModeToggle/>
-        <div className="fixed bottom-0 left-0 flex h-48  p-5 w-full items-end justify-end  lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <ModeToggle/>
-        </div>
-      </div>
+      
+      <NavBar/>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+      <div className="relative flex place-items-center ">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/next.svg"
---
+++ components/NavBar.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image'
+import React from 'react'
+import { ModeToggle } from './ModeToggle'
+
+export const NavBar = () => {
+
+    return (
+        <div className="flex flex-row z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+            <Image
+                src="/logogym.png"
+                alt="Jc personal trainer Logo"
+                className="dark:invert w-16 lg:w-24"
+                width={100}
+                height={24}
+                priority
+            />
+
+            <h3 className="text-lg font-extralight tracking-tight font-serif lg:text-2xl">
+                JC PERSONAL TRAINER
+            </h3>
+
+            <div className="fixed bottom-0 left-0 flex h-48  p-5 w-full items-end justify-end  lg:static lg:h-auto lg:w-auto lg:bg-none">
+                <ModeToggle />
+            </div>
+        </div>
+    )
+}
