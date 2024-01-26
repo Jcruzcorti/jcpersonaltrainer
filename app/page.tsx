626bd307503ae7821c4859e87d932e51d5094a54
Juan Cruz Cortiñas <juancruzcor@hotmail.com>
2024-01-26 15:50:45 -0300

    Creation of Presentation.tsx component to render code in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,13 @@
-import { ModeToggle } from "@/components/ModeToggle";
 import { NavBar } from "@/components/NavBar";
-import Image from "next/image";
+import { Presentation } from "@/components/Presentation";
+
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-16">
-      
+    <main className="flex min-h-screen flex-col items-center justify-between p-16">    
       <NavBar/>
-
-      <div className="relative flex place-items-center ">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <Presentation/>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,17 +5,17 @@ import { ModeToggle } from './ModeToggle'
 export const NavBar = () => {
 
     return (
-        <div className="flex flex-row z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <div className="flex flex-row pb-20 z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
             <Image
                 src="/logogym.png"
                 alt="Jc personal trainer Logo"
-                className="dark:invert w-16 lg:w-24"
+                className="dark:invert w-20 lg:w-24"
                 width={100}
                 height={24}
                 priority
             />
 
-            <h3 className="text-lg font-extralight tracking-tight font-serif lg:text-2xl">
+            <h3 className="text-base font-light tracking-tight font-serif lg:text-2xl">
                 JC PERSONAL TRAINER
             </h3>
 
